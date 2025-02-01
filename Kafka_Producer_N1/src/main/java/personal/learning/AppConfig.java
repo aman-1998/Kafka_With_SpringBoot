30ab6ea42b7d74f1d26d92e178b423785187afbc
Aman Mishra <mishraamankpa@gmail.com>
2025-02-02 00:00:00 +0530

    Kafka Producer

---
+++ Kafka_Producer_N1/src/main/java/personal/learning/AppConfig.java
@@ -0,0 +1,16 @@
+package personal.learning;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import personal.learning.publisher.MessagePublisher;
+
+@Configuration
+public class AppConfig {
+	
+	@Bean
+	public MessagePublisher MessagePublisher() {
+		return new MessagePublisher();
+	}
+	
+}
